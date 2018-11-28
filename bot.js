834b5f84cbd5a101e9b60b700e3554e9f5ca51c6
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-11-28 15:49:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,27 @@ client.on('message', message => {
 });
 
 
-
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'wᴇʟᴄᴏᴍᴇ');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField('🎽 | name :  ',`${member}`)
+        .addField('📢 | نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
+        .addField('🆔 | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                     
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("Gᴀᴍᴇʀs Cᴏᴍᴍᴜɴɪᴛʏ 🎮")
+        .setTimestamp()
+   
+      channel.sendEmbed(embed);
+    });
 
 
 client.login(process.env.BOT_TOKEN);
