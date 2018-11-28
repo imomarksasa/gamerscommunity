18234d33be3627270de5f6ecc534a9780c19c142
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-11-28 16:13:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,29 @@ client.on('message', message => {
       }
 });
 
+client.on("message", message => {
+    
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "gamer")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل ",
+        footer: {
+          text: "©AlaseriDev"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+});
+
 
 client.on('ready', function(){
     var ms = 10000 ;
